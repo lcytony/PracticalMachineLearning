582509afbef4705acbd93a50422506c45c423574
lcytony <chengyun.li@uconn.edu>
2015-08-23 19:29:54 -0400

    Code for Project

--- Run_analysis.R
+++ Run_analysis.R
@@ -1,65 +1,39 @@
-require(data.table)
+library(data.table)
+library(dplyr)
 library(caret)
-setInternet2(TRUE)
-url <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv"
-D <- fread(url)
-url <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv"
-DTest <- fread(url)
-isAnyMissing <- sapply(DTest, function (x) any(is.na(x) | x == ""))
-isPredictor <- !isAnyMissing & grepl("belt|[^(fore)]arm|dumbbell|forearm", names(isAnyMissing))
-predCandidates <- names(isAnyMissing)[isPredictor]
-predCandidates
-varToInclude <- c("classe", predCandidates)
-D <- D[, varToInclude, with=FALSE]
-dim(D)
-names(D)
-D <- D[, classe := factor(D[, classe])]
-D[, .N, classe]
-seed <- as.numeric(as.Date("2014-10-26"))
-set.seed(seed)
-inTrain <- createDataPartition(D$classe, p=0.6)
-DTrain <- D[inTrain[[1]]]
-DProbe <- D[-inTrain[[1]]]
-X <- DTrain[, predCandidates, with=FALSE]
-preProc <- preProcess(X)
-preProc
-XCS <- predict(preProc, X)
-DTrainCS <- data.table(data.frame(classe = DTrain[, classe], XCS))
-X <- DProbe[, predCandidates, with=FALSE]
-XCS <- predict(preProc, X)
-DProbeCS <- data.table(data.frame(classe = DProbe[, classe], XCS))
-nzv <- nearZeroVar(DTrainCS, saveMetrics=TRUE)
-if (any(nzv$nzv)) nzv else message("No variables with near zero variance")
-histGroup <- function (data, regex) {
-  col <- grep(regex, names(data))
-  col <- c(col, which(names(data) == "classe"))
-  require(reshape2)
-  n <- nrow(data)
-  DMelted <- melt(data[, col, with=FALSE][, rownum := seq(1, n)], id.vars=c("rownum", "classe"))
-  require(ggplot2)
-  ggplot(DMelted, aes(x=classe, y=value)) +
-    geom_violin(aes(color=classe, fill=classe), alpha=1/2) +
-    #     geom_jitter(aes(color=classe, fill=classe), alpha=1/10) +
-    #     geom_smooth(aes(group=1), method="gam", color="black", alpha=1/2, size=2) +
-    facet_wrap(~ variable, scale="free_y") +
-    scale_color_brewer(palette="Spectral") +
-    scale_fill_brewer(palette="Spectral") +
-    labs(x="", y="") +
-    theme(legend.position="none")
-}
-histGroup(DTrainCS, "belt")
-histGroup(DTrainCS, "[^(fore)]arm")
-histGroup(DTrainCS, "dumbbell")
-histGroup(DTrainCS, "forearm")
-require(parallel)
-require(doParallel)
-cl <- makeCluster(detectCores() - 1)
-registerDoParallel(cl)
-ctrl <- trainControl(classProbs=TRUE,
-                     savePredictions=TRUE,
-                     allowParallel=TRUE)
-method <- "rf"
-system.time(trainingModel <- train(classe ~ ., data=DTrainCS, method=method))
-stopCluster(cl)
-hat <- predict(trainingModel, DTrainCS)
-confusionMatrix(hat, DTrain[, classe])
+fileUrl="https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv"
+download.file(fileUrl,destfile="e:/uconn/academic/coursera/Practical Machine Learning/ProjectTrain.csv")
+fileUrl="https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv"
+download.file(fileUrl,destfile="e:/uconn/academic/coursera/Practical Machine Learning/ProjectTest.csv")
+Train<-read.csv("e:/uconn/academic/coursera/Practical Machine Learning/ProjectTrain.csv")
+Test<-read.csv("e:/uconn/academic/coursera/Practical Machine Learning/ProjectTest.csv")
+Missing <- sapply(Test, function (x) any(is.na(x) | x == ""))
+Predictor <- names(Test)[!Missing & grepl("belt|arm|dumbbell|forearm", names(Missing))]
+Predictor
+Train<-Train[,c("classe",Predictor)]
+inTrain<-createDataPartition(y=Train$classe,p=0.7,list=FALSE)
+training<-Train[inTrain,]
+testing<-Train[-inTrain,]
+M<-abs(cor(training[,-1]))
+diag(M)<-0
+which(M>0.8,arr.ind=T)
+PreObj<-preProcess(training[,-1])
+trainingSD<-predict(PreObj,training[,-1])
+nzv<-nearZeroVar(trainingSD,saveMetrics=TRUE)
+preProc<-preProcess(trainingSD,method="pca",thresh=0.8)
+trainingPC<-predict(preProc,trainingSD)
+modFit<-randomForest(training$classe~.,data=trainingPC)
+pred<-predict(modFit,trainingPC,type="class")
+training$predictRight<-pred==training$classe
+table(pred,training$classe)
+testingSD<-predict(PreObj,testing[,-1])
+testingPC<-predict(preProc,testingSD)
+predTest<-predict(modFit,testingPC,type="class")
+testing$predictRight<-predTest==testing$classe
+table(predTest,testing$classe)
+
+########### test #########
+Test<-Test[,Predictor]
+testSD<-predict(PreObj,Test)
+testPC<-predict(preProc,testSD)
+predict(modFit,testPC,type="class")
